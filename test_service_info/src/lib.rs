1737480247ee699fd73242cf88b7ba7c21f6524c
Rafael <rafael@iswe.co.nz>
2025-03-19 16:43:29 +1300

    organied mono repo

--- test_service_info/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Test Service Info
-//!
-//! This is a placeholder crate for service information.
-
-/// A placeholder function
-pub fn placeholder() {
-    println!("Test Service Info placeholder");
-}
