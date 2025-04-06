41578d40eabc05ac712157bbc6cd15b49528c3dc
Rafael <rafael@iswe.co.nz>
2025-04-06 15:17:58 +1200

    Remove rust-node-old directory as part of cleanup

--- rust-node-old/src/routing/mod.rs
+++
@@ -1,6 +0,0 @@
-// Module for path routing components
-pub mod topic_path;
-pub mod path_utils;
-
-// Re-export key components
-pub use topic_path::*;
--- rust-node-old/tests/unit/mod.rs
+++
@@ -1,3 +0,0 @@
-// Include all unit test modules
-pub mod service_registry_test;
-pub mod subscriber_management_test; 
